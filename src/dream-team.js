b4e9b0271579f7df780b65d3a7597aadd8742ada
Dmitry Khamitsevich <bullet.xenus@gmail.com>
2020-09-02 23:26:39 +0300

    feat: repair function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  //throw new CustomError('Not implemented');
+  
   if (!Array.isArray(members)) {
     return false;
   }
