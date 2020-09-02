50e106f039f090ce79e8fe699c06a93eeff5bf56
Dmitry Khamitsevich <bullet.xenus@gmail.com>
2020-09-02 16:28:30 +0300

    fix: add realize function count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  let count = matrix.reduce((value, item) => {
+    return value + (item.reduce((value2, item2) => {
+    return (item2 === "^^") ? value2 = value2 = value2 + 1 : value2 += 0;
+    }, 0));
+ }, 0)
+ return count;
   // remove line with error and write your code here
 };
